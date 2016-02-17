8d2d671c3606ff74d3ca4bba26d52207c053e6b0
akshatashinde <mahendra.shoffex@gmail.com>
2016-02-17 13:38:35 +0530

    committing a2.py

---
+++ a2.py
@@ -0,0 +1,9 @@
+print "Enter the name :",
+name=raw_input()
+print "enter the age:",
+age=raw_input()
+print "enter the DOB:",
+dob=raw_input()
+
+print"so u r %r ,date of birth is %r and %r old." %(name,dob,age)
+
